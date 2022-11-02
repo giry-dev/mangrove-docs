fe42574e29aea92989f2bf22b4a9a4c0fa8c10d3
Peter Kejlberg Madsen <peter@mangrove.exchange>
2022-11-02 13:04:34 +0100

    added math plugin

--- mangrove-docs/docusaurus.config.js
+++ mangrove-docs/docusaurus.config.js
@@ -4,6 +4,8 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const simplePlantUML = require("@akebifiky/remark-simple-plantuml");
+const math = require('remark-math');
+const katex = require('rehype-katex');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -36,7 +38,15 @@ const config = {
     
   ],
   staticDirectories: ['static'],
-
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
   presets: [
     [
       'classic',
@@ -44,7 +54,8 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          remarkPlugins: [simplePlantUML]
+          remarkPlugins: [simplePlantUML, math],
+          rehypePlugins: [katex],
         },
         blog: {
           showReadingTime: true,
