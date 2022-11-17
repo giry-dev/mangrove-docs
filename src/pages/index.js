124463955ba0ce8d085c019fc2252a9b59519a51
Espen Højsgaard <espen@espen.dk>
2022-11-17 15:26:04 +0100

    Fix front page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,9 +32,9 @@ export default function Home() {
         </div>
           <h2 style={{margin:'auto', width: 'fit-content', paddingTop: '10px'}}> 🌴 Developer Documentation for Mangrove 🌴</h2>
         <section style={{width: 'fit-content', maxWidth: '800px', marginLeft: 'auto', marginRight: 'auto', paddingTop: '10px'}}>
-          <p>Go to the <a href="./docs/contracts/">Contracts</a> section, if you want to learn more about Mangrove, by delving into the core concepts, finding the API documentation, or because you want to check out the deployment addresses for Mangrove.</p>
-          <p>Strategy builders or market makers that want to develop their own market making strategies should make sure to read about <a href="./docs/strat-lib/">the Mangrove Strat Library</a>  with contract building blocks to help you write safe and efficient market making strats for Mangrove.</p>
-          <p>Off-chain builders that are keen to write bots or UI interfacing with Mangrove should check out the documentation for the <a href="./docs/SDK/">SDK</a> and the dedicated section on <a href="./docs/keeper-bots/">Keeper bots</a>.</p>
+          <p>Go to the <a href="./contracts/">Contracts</a> section, if you want to learn more about Mangrove, by delving into the core concepts, finding the API documentation, or because you want to check out the deployment addresses for Mangrove.</p>
+          <p>Strategy builders or market makers that want to develop their own market making strategies should make sure to read about <a href="./strat-lib/">the Mangrove Strat Library</a>  with contract building blocks to help you write safe and efficient market making strats for Mangrove.</p>
+          <p>Off-chain builders that are keen to write bots or UI interfacing with Mangrove should check out the documentation for the <a href="./SDK/">SDK</a> and the dedicated section on <a href="./keeper-bots/">Keeper bots</a>.</p>
           <p>All GitHub repositories can be found on the <a href="https://github.com/mangrovedao">Mangrove</a> GitHub.</p>
         </section>
       </main>
