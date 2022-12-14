fdfb21c160f0b8bb483228e81636907488b3fff7
Troels Damgaard <troels@mangrove.exchange>
2022-12-14 14:08:16 +0100

    Feat/frontpage matter (#89)
    * design(frontpage): features - bit more padding
    
    * design: frontpage boxed buttons
    
    * design: less margin for frontpage-buttons
    
    * design: better buttons and simplify
    
    * design: frontpage buttons - grid design + colours
    
    * fix: unused comment
    
    * design: smaller icons
    
    * design: Better button color in light mode
    
    * fix: prettify col distribution a bit
    
    * feat: messages on frontpage
    
    Currently no messages
    
    * design: Improve design, when no messages

--- src/components/HomepageSections/index.js
+++ src/components/HomepageSections/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-const FeatureList = [
+const TopFeatureList = [
   {
     title: 'Contracts',
     path: 'contracts',
     Svg: 'img/streamlinehq-phone-contact-phone-book-phone.svg',
     description: (
       <>
-        Go to the <a href="./contracts/">Contracts</a> section, if you want to learn more about Mangrove, by delving into the core concepts, finding the API documentation, or because you want to check out the deployment addresses for Mangrove.
+        Go to the Contracts section to learn more about Mangrove. 
+        Delve into the core concepts, find the API documentation, or check out the deployment addresses for Mangrove.
       </>
     ),
+    elementClass: 'frontpage--button',
   },
   {
     title: 'Strat Lib',
@@ -19,9 +21,10 @@ const FeatureList = [
     Svg: 'img/streamlinehq-money-graph-arrow-increase-money-shopping.svg',
     description: (
       <>
-        Builders that want to develop their own market making strategies should read about the <a href="./strat-lib/">Mangrove Strat Library</a> with contract building blocks to help you write safe and efficient market making strats for Mangrove.
+        Read about how to develop market making strategies with the Mangrove Strat Library that provides contract building blocks to help write safe and efficient market making strats for Mangrove.
       </>
     ),
+    elementClass: 'frontpage--button',
   },
   {
     title: 'SDK',
@@ -29,9 +32,10 @@ const FeatureList = [
     Svg: 'img/streamlinehq-computer-laptop-computer-devices.svg',
     description: (
       <>
-        Off-chain builders that are keen to write bots or UIs interfacing with Mangrove should check out the documentation for the <a href="./SDK/">SDK</a>.
+        Off-chain builders that are keen to write bots or UIs interfacing with Mangrove should check out the documentation for the SDK.
       </>
     ),
+    elementClass: 'frontpage--button',
   },
   {
     title: 'Keeper Bots',
@@ -39,41 +43,75 @@ const FeatureList = [
     Svg: 'img/streamlinehq-computer-robot-computer-devices.svg',
     description: (
       <>
-        Mangrovians who want to contribute to the Mangrove ecosystem by building and running keeper bots should visit the dedicated section on <a href="./keeper-bots/">Keeper bots</a>.
+        Mangrovians who want to contribute to the Mangrove ecosystem by building and running keeper bots should visit the dedicated section on Keeper Bots.
       </>
     ),
+    elementClass: 'frontpage--button',
   },
-];
+]
 
-function Section({Svg, title, path, description}) {
-  return (
-    <div className={clsx('col col--3')}>
-      <a href={path} className={styles.noColorLink}>
-      <div className="text--center">
-        <img src={Svg} className='frontpageSvgButton' />
-      </div>
-      </a>
-      <div className="text--center padding-horiz--md">
-        <a href={path} className={styles.noColorLink}>
+const MessageList = [
+
+]
+
+
+function Section({Svg, title, path = '', description, colStyle = '', elementClass=''}) {
+  const renderInner = () =>
+  {
+    return (
+      <div className={clsx('col ' + colStyle) }>
+        <div className="text--center">
+          <img src={Svg} className='frontpageSvgButton' />
+        </div>
+        <div className="text--center padding-horiz--md">
           <h3>{title}</h3>
-        </a>                
-        <p>{description}</p>
+          <p>{description}</p>
+        </div>
+        <img src="img/streamlinehq-interface-arrows-corner-up-right-interface-essential-gray.svg" className='frontpage--button-linkArrow' />
       </div>
-
-    </div>
-  );
+    );
+  }
+  if(path) {
+    return (
+      <a href={path} className={styles.noColorLink + ' frontpage--button-link' + ' ' + elementClass}>
+        {renderInner()}
+      </a>
+    );
+  }
+  else 
+    return renderInner();
 }
 
 export default function HomepageSections() {
+  const renderMessages = () => {
+    if(MessageList.length > 0){
+      return (
+        <section className={styles.features}>
+          <div className="frontpage--message-container container">
+            <div className="row">
+              {MessageList.map((props, idx) => (
+                <Section key={idx} {...props} />
+              ))}
+            </div>
+          </div>
+        </section>      
+      )}
+      else 
+        return;
+    };
+
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Section key={idx} {...props} />
-          ))}
+    <>
+      <section className={styles.features}>
+        <div className="frontpage--feature-container container">
+          <div className="frontpage--feature-row">
+            {TopFeatureList.map((props, idx) => (
+              <Section key={idx} {...props} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      {renderMessages()}
+    </>
   );
 }
